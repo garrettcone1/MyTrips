6f51b3ba6b6110a37148e60505a900e8601126a3
Garrett Cone <garrettcone1@Garretts-MacBook-Pro.local>
2019-02-11 02:46:42 -0600

    feat: Add multiple additions to UI for better user experience
    Added the extensionView to animate another view for users to begin the sign up process.

--- MyTrips/MyTrips/LoginVC.swift
+++ MyTrips/MyTrips/LoginVC.swift
@@ -9,28 +9,91 @@
 import UIKit
 import AVFoundation
 import AVKit
+import Firebase
 
 class LoginVC: UIViewController {
 
-    // Outlets
+    // Outlets for LoginVC
     @IBOutlet weak var signUpButton: UIButton!
-    
     @IBOutlet weak var clearView: UIView!
     @IBOutlet weak var backgroundVideoView: UIView!
-    
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    // Outlets for popup view
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var newPasswordTextField: UITextField!
+    @IBOutlet weak var reEnterPasswordTextField: UITextField!
+    @IBOutlet weak var finishedButton: UIButton!
+    @IBOutlet var extensionView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
         setUpLayout()
         setUpVideoView()
+        signUpDetailLayout()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+    }
+    
+    @IBAction func signUpButton(_ sender: Any) {
+        
+        animateExtensionView()
+    }
+    
+    @IBAction func finishedSigningUp(_ sender: Any) {
+        
+        exitExtensionView()
+    }
+    
+    private func animateExtensionView() {
+        
+        self.view.addSubview(extensionView)
+        extensionView.center = self.view.center
+        
+        extensionView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+        extensionView.alpha = 0
+        
+        UIView.animate(withDuration: 0.4) {
+            
+            self.extensionView.alpha = 1
+            self.extensionView.transform = CGAffineTransform.identity
+        }
+    }
+    
+    private func exitExtensionView() {
+        
+        UIView.animate(withDuration: 0.3, animations: {
+            self.extensionView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+            self.extensionView.alpha = 0
+            
+        }) { (success: Bool) in
+        
+            self.extensionView.removeFromSuperview()
+        }
+    }
+    
+    // Check if email string entered is a valid email format
+    func isEmailValid(testString: String) -> Bool {
+        
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        
+        return emailTest.evaluate(with: testString)
+    }
+    
+    // Check if password string entered is a valid password format (Has an uppercase, lowercase, a number and is at least 8 characters long)
+    func isPasswordValid(_ password: String) -> Bool {
+        
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$")
+        
+        return passwordTest.evaluate(with: password)
     }
     
     // Set up for background and button layouts
@@ -73,10 +136,31 @@ class LoginVC: UIViewController {
         let player: AVPlayerItem = notification.object as! AVPlayerItem
         player.seek(to: CMTime.zero, completionHandler: nil)
     }
+    
+    // Set up for the Sign Up details view
+    private func signUpDetailLayout() {
+        
+        extensionView.layer.cornerRadius = 10
+        
+        nameTextField.attributedPlaceholder = NSAttributedString(string: "Name", attributes: [NSAttributedString.Key.foregroundColor : UIColor.darkGray])
+        emailTextField.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor : UIColor.darkGray])
+        newPasswordTextField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor : UIColor.darkGray])
+        reEnterPasswordTextField.attributedPlaceholder = NSAttributedString(string: "Re-Enter Password", attributes: [NSAttributedString.Key.foregroundColor : UIColor.darkGray])
+        
+        nameTextField.underlined()
+        emailTextField.underlined()
+        newPasswordTextField.underlined()
+        reEnterPasswordTextField.underlined()
+        
+        finishedButton.layer.cornerRadius = 10
+        finishedButton.layer.borderWidth = 2
+        finishedButton.layer.borderColor = UIColor.white.cgColor
+    }
 }
 
 extension UITextField {
     
+    // This function is reused for both usernameTextField and passwordTextField to create an underline
     func underlined() {
         
         let border = CALayer()
