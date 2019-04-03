837b1f03bc252799004ca962ae9fc1adff3c710b
Garrett Cone <garrettcone1@Garretts-MacBook-Pro.local>
2019-04-02 21:16:56 -0500

    update: Update settings to Swift 5 and add additional files and groups to organize specific content to be displayed within the app

--- MyTrips/AppDelegate.swift
+++ MyTrips/AppDelegate.swift
@@ -17,9 +17,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
+        // Customize UI for GMSAutocompleteViewController
+        let barColor: UIColor = #colorLiteral(red: 0, green: 0.3285208941, blue: 0.5748849511, alpha: 1)
+        let backgroundColor: UIColor = UIColor.white
+        let textColor: UIColor =  UIColor.white
+        UINavigationBar.appearance().barTintColor = barColor
+        UINavigationBar.appearance().tintColor = UIColor.white
+        
+        let searchBarTextAttributes = [NSAttributedString.Key.foregroundColor: textColor, NSAttributedString.Key.font: UIFont(name: "Helvetica Neue", size: 16)]
+        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).defaultTextAttributes = searchBarTextAttributes as [NSAttributedString.Key : Any]
+        
+        let placeholderAttributes = [NSAttributedString.Key.foregroundColor: backgroundColor, NSAttributedString.Key.font: UIFont(name: "Helvetica", size: 15)]
+        // Color of the default search text.
+        let attributedPlaceholder = NSAttributedString(string: "Search", attributes: placeholderAttributes as [NSAttributedString.Key : Any])
+        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).attributedPlaceholder = attributedPlaceholder
+        
         // Set the Google Places API Key
         GMSPlacesClient.provideAPIKey("AIzaSyBQUuNH03XK1tuHxViJP0Es0eOFX_O-Mag")
         
+        // Firebase Listener
         FirebaseApp.configure()
         
         _ = Auth.auth().addStateDidChangeListener { (auth, user) in
@@ -63,7 +79,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
---
+++ MyTrips/MyTrips/HomePage/CityDetailsVC.swift
@@ -0,0 +1,15 @@
+//
+//  CityDetailsVC.swift
+//  MyTrips
+//
+//  Created by Garrett Cone on 4/2/19.
+//  Copyright © 2019 Garrett Cone. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CityDetailsVC: UIViewController {
+    
+    
+}
